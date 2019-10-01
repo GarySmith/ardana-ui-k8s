26b21b383261f4d1a0f2f0d8af038c2ef48e86dd
Gary Smith <gary.smith@suse.com>
2019-09-30 17:48:36 -0700

    Initial commit, deploying just the ui container

---
+++ run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Verify that kubectl is installed and configured to connected to kubernetes
+# Assume minikube is running
+eval $(minikube docker-env)
+
+DIR=$(realpath -m "${BASH_SOURCE[0]}/..")
+cd $DIR
+
+NAME=ardana-installer-ui
+
+mkdir -p svcs
+
+declare -A SERVICES
+SERVICES=(\
+    [ui]=https://github.com/ArdanaCLM/ardana-installer-ui.git \
+    [svc]=https://github.com/ArdanaCLM/ardana-service.git \
+    [key]=https://github.com/GarySmith/docker-keystone.git)
+
+set -x 
+for repo in ${!SERVICES[@]}; do
+    cd $DIR/svcs
+    if [[ ! -d $repo ]] ; then
+        git clone ${SERVICES[$repo]} $repo 
+    fi
+    cd $repo
+    docker build -t ${NAME}-${repo} .
+done
+
+cd $DIR
+
+# create namespace
+kubectl get namespace -o name | grep -q namespace/$NAME 
+if [[ $? -ne 0 ]] ; then
+    kubectl create namespace $NAME
+fi
+
+# apply installation file
+# kubectl apply -n $NAME -f install.yml
+
+# open port forwarding
+# kubctl port-forward service/myservice 2209
